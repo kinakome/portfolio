8e18f0a1f5522505226ff5c515f0c2a656b4ff39
kinakome <ryomatsu0120@gmail.com>
2020-08-20 12:19:30 +0900

    [Test] update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Props = {
+  message: string
 }
 
-export default App;
+const Child: React.FC<Props> = props => {
+  return (
+    <p>{ props.message }</p>
+  )
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Child message="子のコンポーネントに渡す" />
+    </div>
+  )
+}
+
+export default App
