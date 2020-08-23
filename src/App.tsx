05c0c1253e7762098946a98e6f66b24242aee84c
kinakome <ryomatsu0120@gmail.com>
2020-08-23 16:46:33 +0900

    use scss setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
+import './style/style.scss'
+import Top from './components/Top'
 
 type Props = {
-  message: string
-}
 
-const Child: React.FC<Props> = props => {
-  return (
-    <p>{ props.message }</p>
-  )
 }
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Child message="子のコンポーネントに渡す" />
-    </div>
+    <div>
+    <Top />
+</div>
   )
 }
 
---
+++ src/components/Top.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+// 関数コンポーネント（returnに表示するhetmlをかく）
+const Top: React.FC<Props> = () => {
+
+  return (
+    <div>
+      aaaa
+    </div>
+  )
+}
+
+export default Top
