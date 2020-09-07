63a517c7e358888bf20e051a60f4f27a359a8c71
kinakome <ryomatsu0120@gmail.com>
2020-09-07 20:40:34 +0900

    [Update] works layout

--- src/components/SvgReader.tsx
+++ src/components/SvgReader.tsx
@@ -3,6 +3,14 @@ import CameraImg from '../images/camera.svg'
 import LogoSvg from '../images/entrance/logo.svg'
 import TitleSvg from '../images/entrance/title.svg'
 import ButtonSvg from '../images/entrance/button.svg'
+import Dn from '../images/works/dn.svg'
+import Portfolio from '../images/works/portfolio.svg'
+import Bplus from '../images/works/b-plus.svg'
+import IllustChat from '../images/works/illustya.svg'
+import Tameru from '../images/works/tameru.svg'
+import Other from '../images/works/other.svg'
+
+
 
 
 import React from 'react'
@@ -26,4 +34,34 @@ export const Title: React.FC = () => {
 export const Button: React.FC = () => {
   return (
   <img src={ButtonSvg}  alt="button" className="entrance__contents--button"/>)
+}
+
+export const PortfolioIcon: React.FC = () => {
+  return (
+  <img src={Portfolio}  alt="portfolio" />)
+}
+
+export const BplusIcon: React.FC = () => {
+  return (
+  <img src={Bplus}  alt="bplus" />)
+}
+
+export const DnIcon: React.FC = () => {
+  return (
+  <img src={Dn}  alt="dn" />)
+}
+
+export const IllustChatIcon: React.FC = () => {
+  return (
+  <img src={IllustChat}  alt="illustChat" />)
+}
+
+export const TameruIcon: React.FC = () => {
+  return (
+  <img src={Tameru}  alt="tameru" />)
+}
+
+export const OtherIcon: React.FC = () => {
+  return (
+  <img src={Other}  alt="other" />)
 }
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Reveal } from "react-genie";
 import { Animation } from "react-genie-styled-components";
+import { DnIcon, PortfolioIcon, BplusIcon, IllustChatIcon, TameruIcon, OtherIcon } from './SvgReader'
+
 
 const Works: React.FC = () => {
   const lineTime = 700
   const centerTime = 1300
   const titleTime = 1700
+  const workTime = 200
 
 
   return (
@@ -22,7 +25,38 @@ const Works: React.FC = () => {
           </div>
         </Reveal>
       </div>
-      <div className="works__contents"></div>
+      <div className="works__contents">
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <DnIcon />
+          </div>
+        </Reveal>
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <PortfolioIcon />
+          </div>
+        </Reveal>
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <IllustChatIcon />
+          </div>
+        </Reveal>
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <BplusIcon />
+          </div>
+        </Reveal>
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <TameruIcon />
+          </div>
+        </Reveal>
+        <Reveal delay={workTime} animation={Animation.FadeInUp}>
+          <div className="work">
+            <OtherIcon />
+          </div>
+        </Reveal>
+      </div>
     </div>
   )
 }
